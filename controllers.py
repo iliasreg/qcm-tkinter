83e096ea63ed67217b1f57da9f4dba1c379c3927
Yassuiine <elmesbahyyassine080@gmail.com>
2025-09-16 15:01:20 +0200

    menu bar

--- controllers.py
+++ controllers.py
@@ -13,10 +13,11 @@ else :
         print("Your python version is : ",major,minor)
         print("... I guess it will work !")
     import tkinter as tk
-    from tkinter import filedialog 
+    from tkinter import filedialog
 
 import json
 from views import *
+from tkinter import Menu,messagebox
 
 class Controller(ConcreteObserver):
     def __init__(self, root, model):
@@ -24,28 +25,88 @@ class Controller(ConcreteObserver):
         self.root = root
         self.model = model
         self.model.attach(self)
+
+        self.menubar = Menu(self.root)
+        self.root.configure(menu=self.menubar)
         
         self.current_view = None
         self.show_login()
+
+    def setup_menu(self,context="login"):
+        self.menubar.delete(0,"end")
+
+        self.root.unbind_all("<Control-n>")
+        self.root.unbind_all("<Control-p>")
+        self.root.unbind_all("<Control-q>")
+        self.root.unbind_all("<Control-l>")
+        self.root.unbind_all("<Control-h>")
+        self.root.unbind_all("<Control-m>")
+
+        self.file_menu = Menu(self.menubar)
+
+        if context == "login":
+            self.file_menu.add_command(label="Quit",accelerator="Ctrl+q",command=self.exit)
+            self.root.bind_all("<Control-q>", lambda e: self.exit())
+
+        elif context == "main":
+            self.file_menu.add_command(label="New",accelerator="Ctrl+n",command=self.show_create_qcm)
+            self.file_menu.add_command(label="Play", accelerator="Ctrl+P",command=self.show_qcm_list)
+            self.file_menu.add_command(label="Logout", accelerator="Ctrl+L",command=self.logout)
+            self.file_menu.add_separator()
+            self.file_menu.add_command(label="Quit", accelerator="Ctrl+Q",command=self.exit)
+
+            self.root.bind_all("<Control-n>", lambda e: self.show_create_qcm())
+            self.root.bind_all("<Control-p>", lambda e: self.show_qcm_list())
+            self.root.bind_all("<Control-l>", lambda e: self.logout())
+            self.root.bind_all("<Control-q>", lambda e: self.exit())
+
+        elif context == "create_qcm":
+            self.file_menu.add_command(label="Help", accelerator="Ctrl+H",command=self.show_help)
+            self.file_menu.add_command(label="Return to Main Menu", accelerator="Ctrl+M",command=self.show_main_menu)
+            self.file_menu.add_separator()
+            self.file_menu.add_command(label="Quit", accelerator="Ctrl+Q",command=self.exit)
+
+            self.root.bind_all("<Control-h>", lambda e: self.show_help())
+            self.root.bind_all("<Control-m>", lambda e: self.show_main_menu())
+            self.root.bind_all("<Control-q>", lambda e: self.exit())
+
+        elif context == "qcm_list":
+            self.file_menu.add_command(label="Return to Main Menu", accelerator="Ctrl+M",command=self.show_main_menu)
+            self.file_menu.add_separator()
+            self.file_menu.add_command(label="Quit", accelerator="Ctrl+Q",command=self.exit)
+
+            self.root.bind_all("<Control-m>", lambda e: self.show_main_menu())
+            self.root.bind_all("<Control-q>", lambda e: self.exit())
+
+        self.menubar.add_cascade(label="File", menu=self.file_menu)
+
+    def show_help(self):
+        messagebox.showinfo("Help", "Fill in the QCM title, number of questions, then press 'Create Questions'.")
+
+    def exit(self):
+        ans = messagebox.askyesno("Confirm Exit", "Are you sure you want to quit?")
+        if ans:
+            self.root.destroy()
     
-    def show_view(self, view_class, *args):
+    def show_view(self, view_class, *args,context="login"):
         if self.current_view:
             self.current_view.destroy()
         
         self.current_view = view_class(self.root, self, *args)
         self.current_view.pack()
+        self.setup_menu(context)
     
     def show_login(self):
-        self.show_view(LoginView)
+        self.show_view(LoginView,context="login")
     
     def show_main_menu(self):
-        self.show_view(MainMenuView)
+        self.show_view(MainMenuView,context="main")
     
     def show_qcm_list(self):
-        self.show_view(QCMListView)
+        self.show_view(QCMListView,context="qcm_list")
     
     def show_create_qcm(self):
-        self.show_view(CreateQCMView)
+        self.show_view(CreateQCMView,context="create_qcm")
     
     def show_create_questions(self, num_questions, title):
         self.show_view(CreateQuestionsView, num_questions, title)
--- main.py
+++ main.py
@@ -16,6 +16,7 @@ else :
     from tkinter import filedialog 
 
 from tkinter import Menu
+from tkinter import messagebox
 from models import UserModel
 from controllers import Controller
 
@@ -23,15 +24,20 @@ def initialize_menu():
     menubar = Menu(root)
     root.config(menu=menubar)
 
-    file_menu = Menu(menubar)
+    file_menu = Menu(menubar, tearoff=False)  # tearoff=False removes the dashed line
 
-    file_menu.add_command(label='New', underline=0, accelerator="CTRL+N")
-    
+    file_menu.add_command(
+        label='New',
+        underline=0,
+        accelerator="Ctrl+N",
+        command=lambda: print("New file created!")  # replace with your action
+    )
+
     file_menu.add_command(
         label='Exit',
         underline=0, 
-        accelerator="CTRL+Q",
-        command=root.destroy
+        accelerator="Ctrl+Q",
+        command=exit
     )
 
     menubar.add_cascade(
@@ -40,16 +46,20 @@ def initialize_menu():
         underline=0
     )
 
-    #file_menu.bind_all("<Control-n>", lambda x: self.do_something())
-    file_menu.bind_all("<Control-q>", lambda x: root.destroy)
+    # bind shortcuts to root instead of file_menu
+    root.bind_all("<Control-n>", lambda event: print("New file created!"))
+    root.bind_all("<Control-q>", exit)
+
+def exit(event= None):
+    answer = messagebox.askyesno("Confirm Exit", "Are you sure you want to quit?")
+    if answer:
+        root.destroy()
 
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("QCM Application")
     root.geometry("600x400")
-
-    initialize_menu()
 
     model = UserModel()
     controller = Controller(root, model)
